81e7cd6bc6a62e372fe7a0e3ed1eee53ada86ddb
fridascript <frida.r.engman@gmail.com>
2026-01-22 11:31:46 +0100

    adds GET endpoints

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import cors from "cors"
 import express from "express"
+import thoughtsData from "./data.json" with { type: "json" }
+
+let thoughts = thoughtsData
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -12,11 +15,55 @@ app.use(cors())
 app.use(express.json())
 
 // Start defining your routes here
+// app.get("/", (req, res) => {
+//   res.send("Hello Technigo!")
+// })
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
-})
+  res.json ({
+    message: "Happy Thoughts API!",
+    endpoints: [ 
+      {
+       path: "/thoughts",
+       method: "GET",
+       description: "Returns all thoughts",
+       queryParams: "?sort=hearts or ?sort=date"
+    },
+    {
+      path: "/thoughts/:id",
+      method: "GET",
+      description: "Returns a single thought by ID"
+    }
+    ]
+  });
+}); 
+
+app.get("/thoughts", (req, res) => {
+  const { sort } = req.query
+
+  let result = [...thoughts]
+
+  if (sort === "hearts") {
+    result.sort((a, b) => b.hearts - a.hearts)
+  } else if (sort === "date") {
+    result.sort ((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+  }
+
+  res.json(result)
+});
+
+app.get("/thoughts/:id", (req,res) => {
+  const { id } = req.params
+  const thought = thoughts.find((t) => t._id === id)
+
+  if (thought) {
+    res.json(thought)
+  }else {
+    res.status(404).json({error: "Thought not found"})
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
