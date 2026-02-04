7ccaac8ee5ed7c9d664c0c4fdb1d21edbc4d255b
fridascript <frida.r.engman@gmail.com>
2026-02-04 18:00:32 +0100

    Adds user atuhentication with bcrypt

--- server.js
+++ server.js
@@ -3,11 +3,59 @@ import cors from "cors";
 import express from "express";
 import thoughtsData from "./data.json" with { type: "json" };
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 // connect to MongoDB
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happythoughts";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
+
+const UserSchema = new mongoose.Schema({
+  name:{
+    type: String, 
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+}); 
+
+const User = mongoose.model("User", UserSchema);
+
+//authentication middleware (checks if user has valid token)
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      return res.status(401).json({
+        success: false,
+        response: null,
+        message: "please log in"
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+        success: false,
+        response: null,
+        message: "Authentication failed"
+    });
+  }
+};
 
 // defines the port the app will run on
 const port = process.env.PORT || 8080;
@@ -56,6 +104,18 @@ app.get("/", (req, res) => {
   res.json({
     message: "Happy Thoughts API!",
     endpoints: [
+  { 
+        path: "/users",
+        method: "POST",
+        description: "Register a new user",
+        body: { name: "string", email:"string", password:"string"}
+      },
+       { 
+        path: "/sessions",
+        method: "POST",
+        description: "Log in (returns accessToken)",
+        body: { email: "string", password:"string"}
+      },
       {
         path: "/thoughts",
         method: "GET",
@@ -115,6 +175,71 @@ app.get("/thoughts", async (req, res) => {
   }
 });
 
+// route to register a new user
+
+app.post("/users", async (req, res) => {
+  try {
+    const {name, email, password } = req.body;
+
+    const salt = bcrypt.genSaltSync();
+
+    const user = new User({
+      name,
+      email,
+      password: bcrypt.hashSync(password, salt)
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      id: user._id,
+      accessToken: user.accessToken
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "could not create user",
+      errors: err.errors
+    });
+  }
+});
+
+// route to log in (sessions)
+
+app.post("/sessions", async (req,res) => {
+  try {
+    const { email, password } = req.body;
+
+    //find user by email
+    const user = await User.findOne ({ email });
+
+    // check if user exists and password is correct 
+    if (user && bcrypt.compareSync(password, user.password)) {
+      return res.status(200).json({
+        success: true,
+        response: {
+          userId: user._id,
+          name: user.name,
+          email: user.email,
+          accessToken: user.accessToken
+        },
+        message: "Login successful"
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        response: null,
+        message: "Invalid email or password"
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      response: null,
+      message: "Could not log in"
+    });
+  }
+});
+
 // route to get a single thought by ID
 app.get("/thoughts/:id", async (req, res) => {
   try {
@@ -154,7 +279,7 @@ app.get("/thoughts/:id", async (req, res) => {
 });
 
 // route to create a new thought
-app.post("/thoughts", async (req, res) => {
+app.post("/thoughts", authenticateUser, async (req, res) => {
   try {
     const { message } = req.body;
 
@@ -184,7 +309,7 @@ app.post("/thoughts", async (req, res) => {
 });
 
 // route to delete a thought by ID
-app.delete("/thoughts/:id", async (req, res) => {
+app.delete("/thoughts/:id", authenticateUser, async (req, res) => {
   try {
     const { id } = req.params;
     
